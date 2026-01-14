c32a2b3244a7a5f34594969725bd82acbfa24c5f
emmanuel iheanacho <emmanueliheanacho04@gmail.com>
2026-01-14 15:46:48 +0100

    greneral

--- app/components/Inbox.tsx
+++ app/components/Inbox.tsx
@@ -11,17 +11,17 @@ function Inbox() {
 
       <div className=" relative z-10 p-4 flex justify-center items-center text-center gap-8">
         <div className="backdrop-blur px-8 py-8 rounded-xl">
-          <h1 className="text-white font-bold sm:text-2xl text-center lg:text-3xl ">
+          <h1 className="text-white font-bold text-2xl text-center lg:text-3xl ">
             Get The Green in Your Inbox
           </h1>
-          <p className="text-white text-center text-sm max-w-sm mt-4 ">
+          <p className="text-white text-center text-sm max-w-sm mt-2 ">
             Subscribe for exclusive plant care tips, New arrivals and fresh
             deals
           </p>
           <input
             placeholder="Enter your email..."
             type="text"
-            className="bg-white w-40 text-sm px-2 border border-green-800 mt-6 py-2 rounded-lg lg:w-80 "
+            className="bg-white w-40 text-sm px-2 border border-green-800 mt-4 py-2 rounded-lg lg:w-80 "
           />
           <button className=" ml-2 py-2 px-4 bg-green-800 text-white rounded-lg text-sm cursor-pointer
            hover:bg-green-700 transition ease-in-out duration-300">
